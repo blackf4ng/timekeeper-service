44b002f76f78095898f5aace1ea8302efa675bd6
Sean Sun <sean@Seans-MacBook-Air.local>
2025-01-11 00:30:56 -0600

    Setting up Spring and Docker

---
+++ src/main/java/org/timekeeper/TimekeeperApplication.java
@@ -0,0 +1,13 @@
+package org.timekeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimekeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/timekeeper/TimekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package org.timekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimekeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
